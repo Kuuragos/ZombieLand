cc4c143d76ecec06929f221c2d5ad319ee85c58d
Kuuragos <alejandrab.19401@gmail.com>
2019-07-23 15:38:51 -0500

    PrimerEdit

---
+++ Assets/Scripts/Constructores/Move.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    float speed = 5;
+    float eje_X;
+    private Rigidbody mover;
+    private bool canJump;
+
+    void Start()
+    {
+        mover = GetComponent<Rigidbody>();
+    
+    }
+
+    private void FixedUpdate()
+    {
+        if(canJump)
+        {
+            canJump = false;
+            mover.AddForce(0, 5, 0, ForceMode.Impulse);
+
+        }
+           
+    }
+
+    void Update()
+    {
+        eje_X += Input.GetAxis("Mouse X");
+        transform.eulerAngles = new Vector3(0, eje_X, 0);
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+           canJump=true;
+        }
+    }
+}
---
+++ Assets/Scripts/Constructores/MoveCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+    public bool inversionMouse;
+
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mouseX += Input.GetAxis("Mouse X");
+        if (inversionMouse)
+        {
+            mouseY += Input.GetAxis("Mouse Y");
+
+        }
+        else
+        {
+            mouseY -= Input.GetAxis("Mouse Y");
+        }
+        transform.eulerAngles = new Vector3(mouseY, mouseX, 0);
+    }
+}
