371f6d2e26d1ab3417d7d6e216f739943464bf00
Kuuragos <alejandrab.19401@gmail.com>
2019-07-24 15:37:11 -0500

    cambios0.2

--- Assets/Scripts/Constructores/Move.cs
+++ Assets/Scripts/Constructores/Move.cs
@@ -29,27 +29,28 @@ public class Move : MonoBehaviour
     void Update()
     {
         eje_X += Input.GetAxis("Mouse X");
-        transform.eulerAngles = new Vector3(0, eje_X, 0);
-
+        
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += Vector3.left * speed * Time.deltaTime;
+            transform.position -= transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
+            transform.position += transform.right * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += Vector3.back * speed * Time.deltaTime;
+            transform.position -= transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * speed * Time.deltaTime;
+            transform.position += transform.forward * speed * Time.deltaTime;
         }
         if (Input.GetKeyUp(KeyCode.Space))
         {
            canJump=true;
         }
+        transform.eulerAngles = new Vector3(0, eje_X, 0);
+
     }
 }
---
+++ Assets/Scripts/Constructores/NpcTerreno.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcTerreno : MonoBehaviour
+{
+    public GameObject[] cubos;
+    void Start()
+    {
+        cubos = new GameObject[10];
+        for (int i = 0; i < 10; i++)
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            Vector3 v = new Vector3();
+            v.x = Random.Range(5, 30);
+            v.z = Random.Range(5, 30);
+            go.transform.position = v;
+            go.name = i.ToString();
+            
+            cubos[i] = go;
+        }
+
+        void Update()
+        {
+
+        }
+    }
+}
